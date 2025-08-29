272c5cc9d59b749e5e0f5d54a347c864417c4a98
Kristian Pennacchia <clubsport7@hotmail.com>
2025-08-29 11:29:13 +1000

    Created `DismissableInputContainer` to easily make containers dismissable via an outside tap.

---
+++ dismissableinputcontainer.lua
@@ -0,0 +1,49 @@
+local InputContainer = require("ui/widget/container/inputcontainer")
+local Geom = require("ui/geometry")
+local Device = require("device")
+local Screen = Device.screen
+local GestureRange = require("ui/gesturerange")
+local UIManager = require("ui/uimanager")
+
+local DismissableInputContainer = InputContainer:extend{
+	content_container = nil,
+}
+
+function DismissableInputContainer:init()
+	if Device:isTouchDevice() then
+		local range = Geom:new{
+			x = 0,
+			y = 0,
+			w = Screen:getWidth(),
+			h = Screen:getHeight(),
+		}
+
+		self.ges_events = self.ges_events or {}
+		self.ges_events.Tap = {
+			GestureRange:new{
+				ges = "tap",
+				range = range,
+			},
+		}
+	end
+end
+
+local function point_inside_rect(x, y, rect)
+  return x >= rect.x and x < (rect.x + rect.w)
+	 and y >= rect.y and y < (rect.y + rect.h)
+end
+
+function DismissableInputContainer:onTap(arg, ges_ev)
+	if not self.content_container then
+		return false
+	end
+
+	if not point_inside_rect(ges_ev.pos.x, ges_ev.pos.y, self.content_container.dimen) then
+		UIManager:close(self)
+		return true
+	end
+
+	return false
+end
+
+return DismissableInputContainer
