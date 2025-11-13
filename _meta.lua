88c1bc8c2cf746d016a594d2982657013e4ae3ec
Kristian Pennacchia <clubsport7@hotmail.com>
2025-11-13 14:29:57 +1100

    Added version to _meta.

--- _meta.lua
+++ _meta.lua
@@ -3,4 +3,5 @@ return {
 	name = "wordreference",
 	fullname = _("WordReference"),
 	description = _([[Lookup highlighted words on WordReference and show results in a popup.]]),
+	version = "v1.4.0",
 }
--- wordreference_version.lua
+++ wordreference_version.lua
@@ -1,1 +1,8 @@
-return "v1.4.0"
+local DataStorage = require("datastorage")
+
+local plugins_dir = DataStorage:getDataDir() .. "/plugins"
+local plugin_path = plugins_dir .. "/" .. "wordreference.koplugin"
+local meta_file = plugin_path .. "/_meta.lua"
+local _, meta = pcall(dofile, meta_file)
+
+return meta.version
